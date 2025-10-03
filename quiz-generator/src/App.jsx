a9c615f83075c73375093e4c5ee86ff3319f74fe
Deepank Kujur <kujurdeepank@gmail.com>
2025-10-03 18:39:44 +0530

    resolve problem

--- quiz-generator/src/App.jsx
+++ quiz-generator/src/App.jsx
@@ -85,11 +85,13 @@ function App() {
   }
 
   return (
+    <>
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 py-8">
       <div className="container mx-auto px-4">
         {renderScreen()}
       </div>
     </div>
+    </>
   )
 }
 
